bd600891d5dcb187c917c065317c7c4a0a560aec
SounKadeMQ <100895046+SounKadeMQ@users.noreply.github.com>
2023-07-31 16:30:44 +1000

    task 3 done working on task 4
    followed video
    trying to add cell class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        Main window = new Main();
+    }
+
+    class Canvas extends JPanel {
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 720));
+        }
+
+        public void paint( Graphics g) {
+            for (int j=10; j<710; j+=35) {
+                for (int i=10; i<710; i+= 35) {
+                  drawCell(g,i,j,35,35,Color.BLACK,Color.WHITE);
+                }
+            }
+        }
+
+        public void drawCell(Graphics g, int x, int y, int width, int height, Color borderColour, Color fillColour) {
+            g.setColor(fillColour);
+            g.fillRect(x, y, width, height);
+            g.setColor(borderColour);
+            g.drawRect(x, y, width, height);
+        }
+
+    }
+
+    class Cell extends JPanel{
+        public Cell() {
+        }
+
+
+        public void drawCell(Graphics g, int x, int y, int width, int height, Color borderColour, Color fillColour) {
+            g.setColor(fillColour);
+            g.fillRect(x, y, width, height);
+            g.setColor(borderColour);
+            g.drawRect(x, y, width, height);
+        }
+
+    }
+
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
+
     }
 }
 
