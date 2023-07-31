b9cbf4c9f3e655b49fb3889e21db882a5ecb8f50
SounKadeMQ <100895046+SounKadeMQ@users.noreply.github.com>
2023-07-31 18:23:24 +1000

    hover highlight

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,49 @@ public class Main extends JFrame {
     }
 
     public class Canvas extends JPanel {
-        public Canvas() {
+        Point mousePos;
+
+        public Canvas(Point mousePos) {
             setPreferredSize(new Dimension(720, 720));
+            this.mousePos = mousePos;
         }
 
         public void paint(Graphics g) {
-            Grid field = new Grid(35, 720, 10);
+            Grid field = new Grid(35, 720, 10, mousePos);
             field.paint(g);
 
         }
 
-        class Grid{
+        class Grid extends JPanel{
             int size;
             int display;
             int margin;
+            Point mousePos;
 
-            public Grid(int size, int display, int margin) {
+            public Grid(int size, int display, int margin,Point mousePos) {
                 this.size = size;
                 this.display = display;
                 this.margin = margin;
+                this.mousePos = mousePos;
             }
             
         public void paint(Graphics g) {
             for (int j=margin; j<display-margin; j+=size) {
-                for (int i=margin; i<display-margin; i+= size) {
+                for (int i=margin; i<display-margin; i+= size) { 
                     Cell c = new Cell(i,j,35,Color.BLACK,Color.WHITE);
+                    
+                    if(mousePos!=null) {
+                        if(mousePos.x > i & mousePos.x < i+size && mousePos.y > j && mousePos.y < j+size) {
+                            c = new Cell(i,j,35,Color.BLACK,Color.GRAY);
+                        }
+                    }                        
                     c.paint(g);
                 }
             }
         }
 
         }
-        class Cell{
+        class Cell extends JPanel{
             int x; 
             int y;
             int size;
@@ -51,9 +62,10 @@ public class Main extends JFrame {
                 this.borderColour = borderColour;
                 this.fillColour = fillColour;
             }
-        
+            
 
             public void paint(Graphics g) {
+                g.drawRect(x, y, size, size);
                 g.setColor(fillColour);
                 g.fillRect(x, y, size, size);
                 g.setColor(borderColour);
@@ -66,10 +78,17 @@ public class Main extends JFrame {
 
     private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Canvas canvas = new Canvas();
+        Point mousePos = this.getMousePosition();
+        Canvas canvas = new Canvas(mousePos);
         this.setContentPane(canvas);
         this.pack();
         this.setVisible(true);
+        for (int i = 2; i>1; i++) {
+            mousePos = this.getMousePosition();
+            canvas = new Canvas(mousePos);
+            this.setContentPane(canvas);
+            this.setVisible(true);
+        }
 
     }
 }
