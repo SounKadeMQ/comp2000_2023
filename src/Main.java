51e047f10d558d187e2c161000c44f268c9b64b3
SounKadeMQ <100895046+SounKadeMQ@users.noreply.github.com>
2023-07-31 17:06:14 +1000

    created cell and grid classes

--- src/Main.java
+++ src/Main.java
@@ -5,41 +5,64 @@ public class Main extends JFrame {
         Main window = new Main();
     }
 
-    class Canvas extends JPanel {
+    public class Canvas extends JPanel {
         public Canvas() {
             setPreferredSize(new Dimension(720, 720));
         }
 
-        public void paint( Graphics g) {
-            for (int j=10; j<710; j+=35) {
-                for (int i=10; i<710; i+= 35) {
-                  drawCell(g,i,j,35,35,Color.BLACK,Color.WHITE);
+        public void paint(Graphics g) {
+            Grid field = new Grid(35, 720, 10);
+            field.paint(g);
+
+        }
+
+        class Grid{
+            int size;
+            int display;
+            int margin;
+
+            public Grid(int size, int display, int margin) {
+                this.size = size;
+                this.display = display;
+                this.margin = margin;
+            }
+            
+        public void paint(Graphics g) {
+            for (int j=margin; j<display-margin; j+=size) {
+                for (int i=margin; i<display-margin; i+= size) {
+                    Cell c = new Cell(i,j,35,Color.BLACK,Color.WHITE);
+                    c.paint(g);
                 }
             }
         }
 
-        public void drawCell(Graphics g, int x, int y, int width, int height, Color borderColour, Color fillColour) {
-            g.setColor(fillColour);
-            g.fillRect(x, y, width, height);
-            g.setColor(borderColour);
-            g.drawRect(x, y, width, height);
+        }
+        class Cell{
+            int x; 
+            int y;
+            int size;
+            Color borderColour;
+            Color fillColour;
+
+            public Cell(int x, int y, int size, Color borderColour, Color fillColour) {
+                this.x = x; 
+                this.y = y;
+                this.size = size;
+                this.borderColour = borderColour;
+                this.fillColour = fillColour;
+            }
+        
+
+            public void paint(Graphics g) {
+                g.setColor(fillColour);
+                g.fillRect(x, y, size, size);
+                g.setColor(borderColour);
+                g.drawRect(x, y, size, size);
+            }
         }
 
     }
 
-    class Cell extends JPanel{
-        public Cell() {
-        }
-
-
-        public void drawCell(Graphics g, int x, int y, int width, int height, Color borderColour, Color fillColour) {
-            g.setColor(fillColour);
-            g.fillRect(x, y, width, height);
-            g.setColor(borderColour);
-            g.drawRect(x, y, width, height);
-        }
-
-    }
 
     private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
