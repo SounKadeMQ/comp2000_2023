4b1ed92ae1bcb44af7c0a590d2518dfbf137a51a
SounKadeMQ <100895046+SounKadeMQ@users.noreply.github.com>
2023-08-15 16:37:22 +1000

    task 11 complete

--- src/Actor.java
+++ src/Actor.java
@@ -1,15 +1,18 @@
 import java.awt.Color;
 import java.awt.Polygon;
 import java.awt.Graphics;
-
+import java.util.ArrayList;
 public abstract class Actor {
   Color color;
   Cell loc;
+  ArrayList<Polygon> shapes = new ArrayList<>();
 
   public void paint(Graphics g) {
-    g.setColor(color);
-    g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    g.setColor(Color.GRAY);
-    g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    for(int i = 0;i < shapes.size();i++) {
+      g.setColor(color);
+      g.fillPolygon(shapes.get(i));
+      g.setColor(Color.GRAY);
+      g.drawPolygon(shapes.get(i));
+    }
   }
 }
